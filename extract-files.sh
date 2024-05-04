9cfda93d1038aa0ff7d75b314973674172efd7be
ReallySnow <kawaii.loli@reallysnow.moe>
2024-05-04 15:00:53 +0800

    taimen: Update CNE, IMS, QMI, RIL blobs
    * system: LA.QSSI.13.0.r1-05300-qssi.0
      vendor: LA.UM.9.6.3.r1-04400-89xx.0
    * pd-mapper keep stock as it is required for wifi work
    * wahoo has been using Oreo's ril blobs from O to R,
      which is no longer available in 13, so upgrade
      the RIL-related blobs
    
    Change-Id: I76db817c73b620ebbd5e5928ad6d99dca1b0a91f

--- extract-files.sh
+++ extract-files.sh
@@ -64,7 +64,7 @@ function blob_fixup() {
         sed -i 's/qulacomm/qualcomm/' "${2}"
         ;;
     # Fix missing symbols for IMS/Camera
-    lib/lib-imsvideocodec.so | lib/libimsmedia_jni.so | lib64/lib-imsvideocodec.so | lib64/libimsmedia_jni.so)
+    system_ext/lib64/lib-imsvideocodec.so | system_ext/lib64/libimsmedia_jni.so)
         for LIBGUI_SHIM in $(grep -L "libgui_shim.so" "${2}"); do
             "${PATCHELF}" --add-needed "libgui_shim.so" "${LIBGUI_SHIM}"
         done
